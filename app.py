467ac0bbc6f9e561bbcae1aadfcd36fb483272db
saem <saem@kaist.ac.kr>
2022-09-19 21:14:14 +0900

    forth commit

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #%%
 
-import tensorflow as tf
+# import tensorflow as tf
 import streamlit as st
 from PIL import Image
 
@@ -8,7 +8,7 @@ from random import *
 
 import pandas as pd
 
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 from streamlit_option_menu import option_menu
