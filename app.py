3fb20eb3b96db8124e6b9da3567781dbff6a94be
saem <saem@kaist.ac.kr>
2023-02-23 10:16:59 +0900

    Second commit

--- app.py
+++ app.py
@@ -320,8 +320,8 @@ def case_summary() : # 수집된 사례 데이터 집계
         fig_2.index = ['사례 건수(건)']
         # st.write(fig_2)
         # st.write(fig_2.T)
-        # plt.figure(figsize=(10,7))
-        fig_2_1 = fig_2.T.plot(kind='barh', figsize=(10,7))
+        fig_2_1 = plt.figure(figsize=(10,7))
+        fig_2.T.plot(kind='barh')
         plt.ylabel('건물 유형', fontproperties=font)
         plt.yticks(fontproperties=font)
         plt.xticks(fontproperties=font)
