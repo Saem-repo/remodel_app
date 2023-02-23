679e6a9ec8bfa5a3a11bb8dd9709d5f59fc56050
saem <saem@kaist.ac.kr>
2023-02-23 10:03:22 +0900

    Second commit

--- app.py
+++ app.py
@@ -318,7 +318,7 @@ def case_summary() : # 수집된 사례 데이터 집계
         
         fig_2 = pd.DataFrame([build_type], columns=build_type.keys())
         fig_2.index = ['사례 건수(건)']
-        st.write(fig_2)
+        # st.write(fig_2)
         # st.write(fig_2.T)
         # plt.figure(figsize=(10,7))
         fig_2.T.plot(kind='barh', figsize=(10,7))
@@ -327,7 +327,7 @@ def case_summary() : # 수집된 사례 데이터 집계
         plt.xticks(fontproperties=font)
         # plt.legend(fontproperties=font)
         
-        st.pyplot()
+        # st.pyplot()
 
     #     col3, col4 = st.columns(2)            
     #     with col3:
