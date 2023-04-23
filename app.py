f9c9e14f04475aa53f6136b545d0d0ebb651e8fc
saem <saem@kaist.ac.kr>
2023-04-24 00:57:36 +0900

    sixth commit

--- app.py
+++ app.py
@@ -1115,7 +1115,7 @@ def rec_cases ():
                         # IMPORTANT: Cache the conversion to prevent computation on every rerun
                         return input_df.to_html(escape=False, formatters=dict(사진=image_formatter))
 
-                    html = convert_df(result_df.iloc[:,:])
+                    html = convert_df(result_df_new_new.iloc[:,:])
 
                     # html = convert_df(rev_df)
 
