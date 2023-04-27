d9fa70aae502aad98ed75787e1b52c9170818e0c
saem <saem@kaist.ac.kr>
2023-04-27 21:08:39 +0900

    sixth commit

--- app.py
+++ app.py
@@ -1090,10 +1090,11 @@ def rec_cases ():
                     worst_grade_df = result_df.loc[(result_df['energy_grade'] == '7'), ['wall','roof','window','airtight','awning','coolheat','ventilation','lighting','sunlight','solarheat','geothermal','fuelcell','ess']].sum()
                     st.write(worst_grade_df)
                     if not (worst_grade_df==0).all():
+                        
                         st.markdown('### 에너지 효율 등급별(7등급) 리모델링 시공 항목')
                     
-                        worst_grade_df = result_df.loc[(result_df['energy_grade'] == '7'), ['wall','roof','window','airtight','awning','coolheat','ventilation','lighting','sunlight','solarheat','geothermal','fuelcell','ess']].sum()
-                        worst_grade_df.columns = ['벽체단열','지붕단열','창문단열','기밀성강화','차양(외부)','냉난방시스템','환기시스템','조명시스템','태양광','태양열','지열','연료전지','에너지저장시스템']
+                        # worst_grade_df = result_df.loc[(result_df['energy_grade'] == '7'), ['wall','roof','window','airtight','awning','coolheat','ventilation','lighting','sunlight','solarheat','geothermal','fuelcell','ess']].sum()
+                        # worst_grade_df.columns = ['벽체단열','지붕단열','창문단열','기밀성강화','차양(외부)','냉난방시스템','환기시스템','조명시스템','태양광','태양열','지열','연료전지','에너지저장시스템']
                         # st.write(worst_grade_df)
                         fig_2 = worst_grade_df.plot(kind='bar', figsize=(13,10)).figure
                         plt.xticks(rotation=0)
