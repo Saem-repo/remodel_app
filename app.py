2e6f93e7817ce803132b5687477a5f6e7c3ad9a3
saem <saem@kaist.ac.kr>
2023-05-24 19:23:45 +0900

    sixth commit

--- app.py
+++ app.py
@@ -1095,10 +1095,10 @@ def rec_cases ():
                     # st.write((worst_grade_df==0).all())
                     if (worst_grade_df==0).all():
                         st.error("테이블에 값이 없습니다..")     
+                        st.write(result_df)
                                             
                     else :    # 예외가 발생했을 때 실행됨
                         
-                        st.write(result_df)
                         st.markdown('### 에너지 효율 등급별(7등급) 리모델링 시공 항목')
                     
                         worst_grade_df = result_df.loc[(result_df['energy_grade'] == '7'), ['wall','roof','window','airtight','awning','coolheat','ventilation','lighting','sunlight','solarheat','geothermal','fuelcell','ess']].sum()
